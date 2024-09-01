caefc09ea460d1c907c1133a9823d9a999d26dcb
Narendra Reddy <58972366+Narendra-Reddy1@users.noreply.github.com>
2024-09-01 19:57:32 +0530

    #added boiler plate code for network handling  and removed httpClient package

--- Assets/_Game/_Scripts/NetworkHandler.cs
+++ Assets/_Game/_Scripts/NetworkHandler.cs
@@ -1,7 +1,7 @@
+using System.Net.Http;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.Http;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -20,43 +20,98 @@ public static class NetworkHandler
     {
         public Dictionary<string, string> headers;
         public string body;
+        public string contentType = "application/json";
         public Method method;
     }
-
-    public static  async void FetchData(string url, Action onSuccess, Action onFail, RequestData requestData = null)
+    static readonly HttpClient client = new HttpClient();
+    public static void Init()
     {
-        using (HttpClient client = new HttpClient())
+
+    }
+
+    public static async void Fetch(string url, Action onSuccess, Action onFail, RequestData requestData = null)
+    {
+        HttpRequestMessage request = _GetHttpRequest(url, requestData);
+        HttpResponseMessage response = await client.SendAsync(request);
+        try
         {
-            if (requestData != null)
-                if (requestData.headers.Count > 0)
-                    foreach (var kvp in requestData.headers)
-                        client.DefaultRequestHeaders.Add(kvp.Key, kvp.Value);
-
-            UnityWebRequest request = new UnityWebRequest();
-            
-            switch (requestData.method)
-            {
-                case Method.GET:
-                    break;
-                case Method.POST:
-                    break;
-                case Method.PUT:
-                    break;
-                case Method.PATCH:
-                    break;
-                case Method.DELETE:
-                    break;
-            }
+            response.EnsureSuccessStatusCode();
+            onSuccess?.Invoke();
         }
-
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            Debug.Log($"Status code: {response.StatusCode} phrase: {response.ReasonPhrase}");
+            onFail?.Invoke();
+        }
     }
 
-    public static void FetchData<T>(string url, Action<T> onSuccess, Action onFail, RequestData requestData = null)
+    public static async void Fetch<T>(string url, Action<T> onSuccess, Action onFail, RequestData requestData = null)
     {
-
+        HttpRequestMessage request = _GetHttpRequest(url, requestData);
+        HttpResponseMessage response = await client.SendAsync(request);
+        try
+        {
+            response.EnsureSuccessStatusCode();
+            string data = await response.Content.ReadAsStringAsync();
+            onSuccess?.Invoke(JsonUtility.FromJson<T>(data));
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            Debug.Log($"Status code: {response.StatusCode} phrase: {response.ReasonPhrase}");
+            onFail?.Invoke();
+        }
     }
-    public static void FetchData<T>(string url, Action<T> onSuccess, Action<string> onFail, RequestData requestData = null)
+    public static async void Fetch<T>(string url, Action<T> onSuccess, Action<string> onFail, RequestData requestData = null)
     {
+        HttpRequestMessage request = _GetHttpRequest(url, requestData);
+        HttpResponseMessage response = await client.SendAsync(request);
+        try
+        {
+            response.EnsureSuccessStatusCode();
+            string data = await response.Content.ReadAsStringAsync();
+            onSuccess?.Invoke(JsonUtility.FromJson<T>(data));
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            Debug.Log($"Status code: {response.StatusCode} phrase: {response.ReasonPhrase}");
+            onFail?.Invoke(e.ToString());
+        }
+    }
+
+    private static HttpRequestMessage _GetHttpRequest(string url, RequestData requestData)
+    {
+        HttpRequestMessage request = new HttpRequestMessage();
+        switch (requestData.method)
+        {
+            case Method.GET:
+                request.Method = HttpMethod.Get;
+                break;
+            case Method.POST:
+                request.Method = HttpMethod.Post;
+                break;
+            case Method.PUT:
+                request.Method = HttpMethod.Put;
+                break;
+            case Method.PATCH:
+                request.Method = HttpMethod.Patch;
+                break;
+            case Method.DELETE:
+                request.Method = HttpMethod.Delete;
+                break;
+        }
+        if (requestData.body != null)
+            request.Content = new StringContent(requestData.body, System.Text.Encoding.UTF8, requestData.contentType);
+        if (requestData.headers != null)
+            foreach (var kvp in requestData.headers)
+            {
+                request.Headers.Add(kvp.Key, kvp.Value);
+            }
+        request.RequestUri = new Uri(url);
+        return request;
 
     }
 }
